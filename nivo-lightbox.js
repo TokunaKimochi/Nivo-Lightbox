0c3dc36116cdf5a1128b6d16b84d7c7ec209df52
TokunaKimochi <tokunakimochi@gmail.com>
2016-03-06 16:24:03 +0900

    class name を使用してより動きを付ける

--- nivo-lightbox.js
+++ nivo-lightbox.js
@@ -8,6 +8,7 @@
  */
 
 ;(function($, window, document, undefined){
+    'use strict';
 
     var pluginName = 'nivoLightbox',
         defaults = {
@@ -89,7 +90,6 @@
 			this.processContent( content, currentLink );
 
             // Nav
-            prevNextFlag = null;
             if(this.$el.attr('data-lightbox-gallery')){
                 var galleryItems = $('[data-lightbox-gallery="'+ this.$el.attr('data-lightbox-gallery') +'"]');
 
@@ -99,22 +99,28 @@
                 $('.nivo-lightbox-prev').off('click').on('click', function(e){
                     e.preventDefault();
                     prevNextFlag = 'P';
+                    $this.contentFrameOutEffect();
                     var index = galleryItems.index(currentLink);
                     currentLink = galleryItems.eq(index - 1);
                     if(!$(currentLink).length) currentLink = galleryItems.last();
-                    $this.processContent(content, currentLink);
-                    $this.options.onPrev.call(this, [ currentLink ]);
+                    setTimeout(function(){
+                        $this.processContent(content, currentLink);
+                        $this.options.onPrev.call(this, [ currentLink ]);
+                    }, 250);
                 });
 
                 // Next
                 $('.nivo-lightbox-next').off('click').on('click', function(e){
                     e.preventDefault();
                     prevNextFlag = 'N';
+                    $this.contentFrameOutEffect();
                     var index = galleryItems.index(currentLink);
                     currentLink = galleryItems.eq(index + 1);
                     if(!$(currentLink).length) currentLink = galleryItems.first();
-                    $this.processContent(content, currentLink);
-                    $this.options.onNext.call(this, [ currentLink ]);
+                    setTimeout(function(){
+                        $this.processContent(content, currentLink);
+                        $this.options.onNext.call(this, [ currentLink ]);
+                    }, 250);
                 });
             }
 
@@ -158,8 +164,9 @@
                 video = href.match(/(youtube|youtu|vimeo)\.(com|be)\/(watch\?v=([\w-]+)|([\w-]+))/),
                 nLBoxWrap = $('.nivo-lightbox-wrap');
 
-            nLBoxWrap.css('height', '80%');
+            nLBoxWrap.removeClass('nivo-lightbox-content-zoom-in-div').addClass('nivo-lightbox-content-zoom-out-div');
             content.html('').addClass('nivo-lightbox-loading');
+            $('body').addClass('nivo-lightbox-anchorage-body');
 
             // Is HiDPI?
             if(this.isHidpi() && link.attr('data-lightbox-hidpi')){
@@ -170,18 +177,30 @@
             if(href.match(/\.(jpeg|jpg|gif|png)$/i) !== null){
                 var img = $('<img>', { src: href });
                 img.one('load', function() {
-					var wrap = $('<div class="nivo-lightbox-image" />');
+					var wrap = $('<div />', {
+                        id: 'nivo-lightbox-alive-flow-content',
+                        'class': 'nivo-lightbox-image'
+                    });
                     wrap.append(img);
 					content.html(wrap).removeClass('nivo-lightbox-loading');
 
-					nLBoxWrap.css('height', 'auto');
+					nLBoxWrap.removeClass('nivo-lightbox-content-zoom-out-div').addClass('nivo-lightbox-content-zoom-in-div');
 					$(window).resize(function() {
-						nLBoxWrap.css('height', 'auto');
+						nLBoxWrap.removeClass('nivo-lightbox-content-zoom-out-div').addClass('nivo-lightbox-content-zoom-in-div');
 					});
 
-                    $this.prevNextContentEffect( wrap );
-
-                    $('body').css('overflow', 'hidden');
+                    $this.contentFrameInEffect( wrap );
+                    wrap.off('click').on('click', function(e){
+                        e.preventDefault();
+                        if(nLBoxWrap.hasClass('nivo-lightbox-content-zoom-in-div')){
+                            nLBoxWrap.removeClass('nivo-lightbox-content-zoom-in-div').addClass('nivo-lightbox-content-zoom-out-div');
+                            $('#nivo-lightbox-alive-flow-content').addClass('nivo-lightbox-image-zoom-out-div');
+                        }
+                        else {
+                            nLBoxWrap.removeClass('nivo-lightbox-content-zoom-out-div').addClass('nivo-lightbox-content-zoom-in-div');
+                            $('#nivo-lightbox-alive-flow-content').removeClass('nivo-lightbox-image-zoom-out-div');
+                        }
+                    });
 				}).each(function() {
 					if(this.complete) $(this).load();
 				});
@@ -189,7 +208,7 @@
 				img.error(function() {
 					var wrap = $('<div class="nivo-lightbox-error"><p>'+ $this.options.errorMessage +'</p></div>');
                     content.html(wrap).removeClass('nivo-lightbox-loading');
-                    $this.prevNextContentEffect( wrap );
+                    $this.contentFrameInEffect( wrap );
 				});
             }
             // Video (Youtube/Vimeo)
@@ -213,6 +232,7 @@
                 if(src){
                     var iframeVideo = $('<iframe>', {
                         src: src,
+                        id: 'nivo-lightbox-alive-flow-content',
                         'class': classTerm,
                         frameborder: 0,
                         vspace: 0,
@@ -222,7 +242,7 @@
                     content.html(iframeVideo);
                     iframeVideo.load(function(){
                         content.removeClass('nivo-lightbox-loading');
-                        $this.prevNextContentEffect( iframeVideo );
+                        $this.contentFrameInEffect( iframeVideo );
                     });
                 }
             }
@@ -232,7 +252,10 @@
 					url: href,
 					cache: false,
 					success: function(data) {
-						var wrap = $('<div class="nivo-lightbox-ajax" />');
+						var wrap = $('<div />', {
+                            id: 'nivo-lightbox-alive-flow-content',
+                            'class': 'nivo-lightbox-ajax'
+                        });
 						wrap.append(data);
 						content.html(wrap).removeClass('nivo-lightbox-loading');
 
@@ -254,19 +277,22 @@
 							}
 						});
 
-                       $this.prevNextContentEffect( wrap );
+                       $this.contentFrameInEffect( wrap );
 					},
 					error: function(){
 						var wrap = $('<div class="nivo-lightbox-error"><p>'+ $this.options.errorMessage +'</p></div>');
                         content.html(wrap).removeClass('nivo-lightbox-loading');
-                        $this.prevNextContentEffect( wrap );
+                        $this.contentFrameInEffect( wrap );
 					}
 				});
             }
             // Inline HTML
             else if(href.substring(0, 1) == '#' && link.attr('data-lightbox-type') == 'inline'){
                 if($(href).length){
-                    var wrap = $('<div class="nivo-lightbox-inline" />');
+                    var wrap = $('<div />', {
+                        id: 'nivo-lightbox-alive-flow-content',
+                        'class': 'nivo-lightbox-inline'
+                    });
 					wrap.append($(href).clone().show());
                     content.html(wrap).removeClass('nivo-lightbox-loading');
 
@@ -288,17 +314,18 @@
 						}
 					});
 
-                    $this.prevNextContentEffect( wrap );
+                    $this.contentFrameInEffect( wrap );
 				} else {
 					var wrapError = $('<div class="nivo-lightbox-error"><p>'+ $this.options.errorMessage +'</p></div>');
                     content.html(wrapError).removeClass('nivo-lightbox-loading');
-                    $this.prevNextContentEffect( wrap );
+                    $this.contentFrameInEffect( wrap );
 				}
             }
             // iFrame (default)
             else if(link.attr('data-lightbox-type') == 'iframe'){
                 var iframe = $('<iframe>', {
                     src: href,
+                    id: 'nivo-lightbox-alive-flow-content',
                     'class': 'nivo-lightbox-item',
                     frameborder: 0,
                     vspace: 0,
@@ -308,7 +335,7 @@
                 content.html(iframe);
                 iframe.load(function(){
                     content.removeClass('nivo-lightbox-loading');
-                    $this.prevNextContentEffect( iframe );
+                    $this.contentFrameInEffect( iframe );
                 });
             } else {
 				return false;
@@ -349,7 +376,7 @@
                 overlay.on('click', function(e){
                     if(e.target === this || $(e.target).hasClass('nivo-lightbox-content') || $(e.target).hasClass('nivo-lightbox-image')){
                         $this.destructLightbox();
-                        $('body').css('overflow', 'visible');
+                        $('body').removeClass('nivo-lightbox-anchorage-body');
                     }
                 });
             }
@@ -357,7 +384,7 @@
             close.on('click', function(e){
                 e.preventDefault();
                 $this.destructLightbox();
-                $('body').css('overflow', 'visible');
+                $('body').removeClass('nivo-lightbox-anchorage-body');
             });
 
             return overlay;
@@ -398,7 +425,16 @@
 			return false;
 		},
 
-        prevNextContentEffect: function( content ){
+        contentFrameOutEffect: function(){
+            if (prevNextFlag === 'P') {
+                $('#nivo-lightbox-alive-flow-content').removeClass('nivo-lightbox-prev-content-effect nivo-lightbox-next-content-effect').addClass('nivo-lightbox-pre-prev-content-effect');
+            }
+            else if (prevNextFlag === 'N') {
+                $('#nivo-lightbox-alive-flow-content').removeClass('nivo-lightbox-prev-content-effect nivo-lightbox-next-content-effect').addClass('nivo-lightbox-pre-next-content-effect');
+            }
+        },
+
+        contentFrameInEffect: function( content ){
             if (prevNextFlag === 'P') {
                 content.addClass('nivo-lightbox-prev-content-effect');
             }
